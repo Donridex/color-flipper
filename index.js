2852e395e951ce6b8ae8d4847ce8e060552e0e30
Adesokan Ridwan <donridextech@gmail.com>
2023-09-18 05:58:24 +0100

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log('meeting successful')
 
 const clickMe = document.getElementById('button')
+const spanColor = document.getElementById('spanColor')
 const container = document.querySelector('.container')
 const colors = ["red", "green", "grey",
     "blue", "yellow", "burlywood", "orange", "magenta", "cyan", "brown", "puple", "pink"]
@@ -10,6 +11,7 @@ const colors = ["red", "green", "grey",
     
     generateColor = Math.floor(Math.random() * colors.length)
     container.style.backgroundColor = colors[generateColor]
+    spanColor.innerHTML = colors[generateColor]
  })
 
 
